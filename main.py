fdbb9e446583d79371e4abb3f6ee583c3ccc930c
Deema Rain <deemarain@Deemas-MacBook-Pro.local>
2024-01-02 04:55:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def bio(name,born,pronoun):
+    age = 2024 -born
+    future = born+67
+    print(name, 'was born in', born)
+    print(pronoun, 'will turn', age , 'years old this year')
+    print(pronoun, 'will be 67 in the year', future)
+
+
+   # THE BIO
+bio("adam",1990,"he")
+bio("mohammed", 2004, "he")
+bio("ali", 2007, "he")
+
+
+
+
